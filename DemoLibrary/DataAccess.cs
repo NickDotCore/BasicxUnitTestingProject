2adef8d40c02a9dd5bf0d6a665cc9fc6f84ec373
Nick Browning <56777652+nickbrowningdev@users.noreply.github.com>
2021-04-30 11:57:32 +1000

    Add files via upload

---
+++ DemoLibrary/DataAccess.cs
@@ -0,0 +1,70 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class DataAccess
+    {
+        private static string personTextFile = @"\\vsfile01\Personal\Nicholas Browning\Documents\Other Projects\xUnitTutorial\UserInterface\PersonText.txt";
+
+        public static void AddNewPerson(Person person)
+        {
+            List<Person> people = GetAllPeople();
+
+            AddPersonToPeopleList(people, person);
+
+            List<string> lines = ConvertModelsToCSV(people);
+
+            File.WriteAllLines(personTextFile, lines);
+        }
+
+        public static void AddPersonToPeopleList(List<Person> people, Person person)
+        {
+            if (string.IsNullOrWhiteSpace(person.FirstName))
+            {
+                throw new ArgumentException("You passed in an invalid parameter", "FirstName");
+            }
+
+            if (string.IsNullOrWhiteSpace(person.LastName))
+            {
+                throw new ArgumentException("You passed in an invalid parameter", "LastName");
+            }
+
+            people.Add(person);
+        }
+
+        public static List<string> ConvertModelsToCSV(List<Person> people)
+        {
+            List<string> output = new List<string>();
+
+            foreach (Person user in people)
+            {
+                output.Add($"{ user.FirstName },{ user.LastName }");
+            }
+
+            return output;
+        }
+
+        public static List<Person> GetAllPeople()
+        {
+            List<Person> output = new List<Person>();
+            string[] content = File.ReadAllLines(personTextFile);
+
+            foreach (string line in content)
+            {
+                string[] data = line.Split(',');
+                output.Add(new Person { FirstName = data[0], LastName = data[1] });
+            }
+
+            return output;
+        }
+    }
+}
